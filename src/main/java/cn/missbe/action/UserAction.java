e4005f220d8f309530e3d6d475952b7a3c9a4b6f
missbe <love1208tt@foxmail.com>
2016-10-16 18:40:35 +0800

    项目总体部分结束，进入测试修改BUG阶段

--- src/main/java/cn/missbe/action/UserAction.java
+++ src/main/java/cn/missbe/action/UserAction.java
@@ -5,6 +5,8 @@ import org.apache.struts2.ServletActionContext;
 
 import com.opensymphony.xwork2.ActionSupport;
 
+import cn.missbe.util.PropertiesUtil;
+
 
 public class UserAction extends ActionSupport {
 	private static final long serialVersionUID = 1L;
@@ -18,16 +20,27 @@ public class UserAction extends ActionSupport {
 		if(null == username || null == userpass){
 			return "input";
 		}
-		if(username.equals("admin") && userpass.equals("admin")){
+		String name=PropertiesUtil.getValue("username");
+		String pass=PropertiesUtil.getValue("password");
+		if(null==name ||null==pass){
+			ServletActionContext.getRequest().setAttribute("message", "--管理员开小差了，用户名或者密码丢失--");
+			return "tooltip";
+		}
+		if(username.equals(name) && userpass.equals(pass)){
 		
 			ServletActionContext.getRequest().setAttribute("message", "测试成功完成！");
-
+			ServletActionContext.getRequest().getSession().setAttribute("admin", "admin");
 			return SUCCESS;
 		}else{
 			System.out.println("未查询到该用户");
 			ServletActionContext.getRequest().setAttribute("message","用户名或者密码不正确^_^");
 			return INPUT;
 		}	  
+	}
+	
+	public String logout(){
+		ServletActionContext.getRequest().getSession().invalidate();
+		return SUCCESS;
 	}
 	public String getUserpass() {
 		return userpass;
